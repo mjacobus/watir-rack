9874c50e5c9d7e8228694911b400ad0e92e44a7a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-05-10 12:10:34 -0300

    Adapt library to work with Hanami framework (#1)

--- lib/watir/hanami/browser.rb
+++ lib/watir/hanami/browser.rb
@@ -1,22 +1,21 @@
 module Watir
-  # Reopened Watir::Browser class for working with Rails
+  # Reopened Watir::Browser class for working with Hanami
   class Browser
     # @private
     alias_method :_original_initialize, :initialize
 
-    # Will start Rails instance for Watir automatically and then invoke the
+    # Will start Hanami instance for Watir automatically and then invoke the
     # original Watir::Browser#initialize method.
     def initialize(*args)
-      Rails.boot
+      Hanami.boot
       _original_initialize *args
-      add_exception_hook unless Rails.ignore_exceptions?
     end
 
     # @private
     alias_method :_original_goto, :goto
 
     # Opens the url with the browser instance.
-    # Will add {Rails.host} and {Rails.port} to the url when path is specified.
+    # Will add {Hanami.host} and {Hanami.port} to the url when path is specified.
     #
     # @example Go to the regular url:
     #   browser.goto "http://google.com"
@@ -26,19 +25,8 @@ module Watir
     #
     # @param [String] url URL to be navigated to.
     def goto(url)
-      url = "http://#{Rails.host}:#{Rails.port}#{url}" unless url =~ %r{^(about|data|https?):}i
+      url = "http://#{Hanami.host}:#{Hanami.port}#{url}" unless url =~ %r{^(about|data|https?):}i
       _original_goto url
-    end
-
-    private
-
-    def add_exception_hook
-      after_hooks.add do
-        if error = Rails.error
-          Rails.error = nil
-          raise error
-        end
-      end
     end
   end
 end
--- spec/watir/hanami/browser_spec.rb
+++ spec/watir/hanami/browser_spec.rb
@@ -1,39 +1,25 @@
 require "spec_helper"
 
 describe Watir::Browser do
-  before { allow(Watir::Rails).to receive_messages(ignore_exceptions?: true) }
+  before { allow(Watir::Hanami).to receive_messages(ignore_exceptions?: true) }
 
   context "#initialize" do
-    it "starts Rails before opening the browser" do
-      expect(Watir::Rails).to receive(:boot)
+    it "starts Hanami before opening the browser" do
+      expect(Watir::Hanami).to receive(:boot)
       expect_any_instance_of(Watir::Browser).to receive(:_original_initialize).and_call_original
 
-      Watir::Browser.new
-    end
-
-    it "does not add Exception hook when exceptions are ignored" do
-      allow(Watir::Rails).to receive_messages(ignore_exceptions?: true, boot: nil)
-
-      expect_any_instance_of(Watir::Browser).not_to receive(:add_exception_hook)
-      Watir::Browser.new
-    end
-
-    it "adds Exception hook when exceptions are not ignored" do
-      allow(Watir::Rails).to receive_messages(ignore_exceptions?: false, boot: nil)
-
-      expect_any_instance_of(Watir::Browser).to receive(:add_exception_hook)
       Watir::Browser.new
     end
   end
 
   context "#goto" do
     before do
-      allow(Watir::Rails).to receive_messages(host: "foo.com", port: 42, boot: nil)
+      allow(Watir::Hanami).to receive_messages(host: "foo.com", port: 42, boot: nil)
     end
 
     let(:browser) { Watir::Browser.new }
 
-    it "uses Rails for paths specified as an url" do
+    it "uses Hanami for paths specified as an url" do
       expect(browser).to receive(:_original_goto).with("http://foo.com:42/foo/bar")
       browser.goto("/foo/bar")
     end
